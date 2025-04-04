2978b8166136313877a0421bf55c8e7ddbf50e10
ducksfloat <ducksfloat@gmail.com>
2025-04-03 19:56:23 -0500

    continued building out data cleaning

--- 00_prepData.R
+++ 00_prepData.R
@@ -1,8 +1,15 @@
 
 # load libraries and set options----------------------------------------------------------
-pacman::p_load(tidyverse, sf, rjson)
-
+pacman::p_load(stringr, dplyr, sf, rjson, here, jsonlite)
+setwd(here())
 
 # read in warehouse data --------------------------------------------------
-json_fil <- 
-json_data <- fromJSON(file=json_file)
+jsonFiles <- list.files("data/slack/urban-canopy-log")
+cfscSlack <- data.frame()
+for (i in jsonFiles) {
+  fileName <- i
+  jsonData <- fromJSON(txt = paste0("data/slack/urban-canopy-log/", fileName))%>%
+    select(text)%>%
+    mutate(date = str_replace(fileName,".json",""))
+  cfscSlack <- bind_rows(cfscSlack, jsonData)
+}
