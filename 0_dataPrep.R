427aa7fa028033561544be6c35e8ec9c41829cd4
carina <ducksfloat@proton.me>
2025-05-02 21:58:06 -0500

    data prep

--- 0_dataPrep.R
+++ 0_dataPrep.R
@@ -43,10 +43,10 @@ for (i in jsonFiles) {
     mutate(date = str_replace(fileName,".json",""))
   cfscSlack <- bind_rows(cfscSlack, jsonData)
 }
-write_rds(cfscSlack, "data/slack/cfsc_warehouseSlack.rds")
+write_rds(cfscSlack, "data/slack/cfsc_keystoneSlack.rds")
 
 # read in raw file for cleaning -------------------------------------------
-cfscSlack <- read_rds("data/slack/cfsc_warehouseSlack.rds")
+cfscSlack <- read_rds("data/slack/cfsc_keystoneSlack.rds")
 
 # start parsing and cleaning ----------------------------------------------
 rescueFrom <- c("WP Aldi", "Aldi WP", "Aldi Hodgkins", "Aldi in Wicker Park", "Wicker Park", "Cicero Aldi", "Kostner Aldi",
@@ -58,7 +58,7 @@ rescueFrom <- c("WP Aldi", "Aldi WP", "Aldi Hodgkins", "Aldi in Wicker Park", "W
                 "Marianos w loop", "S.Loop Marianos", "Marianos West Loop", "SL Mariano's", "South Loop Mariano's",
                 
                 "local foods", "Local Foods", "Sysco", "Jewel","Bimbo", "BIMBO", "bimbo", "Windy City", 
-                "MEANS database", "Marillac", "Midwest Foods", "Cold Chain", "LSFM", "PFP",
+                "MEANS database", "Marillac", "Midwest Foods", "Cold Chain", "LSFM", "Logan Square Farmers Market", "PFP",
                 "Green City Market", "Green city", "Green City", "green city market", "Dom's", "Chef’s Warehouse",
                 
                 "West Suburban Pantry", "West Suburban Community Pantry", "Friendship Center",
@@ -106,13 +106,14 @@ cfscSlack_clean1<- cfscSlack_clean0%>%
                          rescueFrom %in% c("IPCFP", "Irving Park pantry", "Irving Park Food Pantry") ~ "Irving Park Pantry",
                          rescueFrom %in% c("Green city", "Green City", "green city market") ~ "Green City Market",
                          rescueFrom %in% c("local foods") ~ "Local Foods",
+                         rescueFrom %in% c("LSFM") ~ "Logan Square Farmers Market",
                          rescueFrom %in% c("West Suburban Community Pantry", "West Suburban Pantry") ~ "Western Suburban Pantry",
                          TRUE ~ rescueFrom),
          takenTo = case_when(takenTo %in% c("Shelter", "shelter") ~ "Pilsen shelter",
                              takenTo %in% c("55th &amp Pulaski") ~ "55/Pulaski",
                              takenTo %in% c("Pilsen &amp Pulaski", "Port &amp Pulaski") ~ "Pilsen/Pulaski",
                              takenTo %in% c("Love Fridges", "Love fridge", "love fridge", "LoveFridge", "community fridges",
-                                            "Lie fridge", "Live Fridge","Live fridge", "community fridges") ~ "Love Fridge",
+                                            "Lie fridge", "Live Fridge","Live fridge", "community fridges", "LF") ~ "Love Fridge",
                              takenTo %in% c("Psn") ~ "PSN",
                              takenTo %in% c("Lsrsn", "lsrsn", "LSRSN ") ~ "LSRSN",
                              takenTo %in% c("pilsen food pantry") ~ "Pilsen Food Pantry",
@@ -124,25 +125,34 @@ cfscSlack_clean1<- cfscSlack_clean0%>%
 check1 <- cfscSlack_clean1%>%
   group_by(rescueFrom)%>%
   summarize(n=n())
-write_xlsx(check1, "data/slack/keystone/rescueFrom.xlsx")
+write_xlsx(check1, "data/slack/rescueFrom.xlsx")
 check2 <- cfscSlack_clean1%>%
   group_by(takenTo)%>%
   summarize(n=n())
-write_xlsx(check2, "data/slack/keystone/takenTo.xlsx")
+write_xlsx(check2, "data/slack/takenTo.xlsx")
 check3 <- cfscSlack_clean1%>%
   group_by(both)%>%
   summarize(n=n())
-write_xlsx(check3, "data/slack/keystone/both.xlsx")
+write_xlsx(check3, "data/slack/both.xlsx")
+
+locations <- bind_rows(check1, check2, check3)%>%
+  filter(!is.na(rescueFrom)|!is.na(takenTo)|!is.na(both))%>%
+  mutate(name = case_when(!is.na(rescueFrom) ~ rescueFrom,
+                          !is.na(takenTo) ~ takenTo,
+                          !is.na(both) ~ both),
+         City = "Chicago",
+         State = "IL")%>%
+  filter(!name %in% c("Mariano’s", "Dom's", ))%>%
+  mutate(pct_n = round((n/sum(n, na.rm = TRUE))*100, 2))%>%
+  select(-c(rescueFrom, takenTo, both))%>%
+  mutate(Address = paste(Name, City, State, sep = ", "))
+
 ggmap::register_google("AIzaSyAwz2wwo1PfDG-zAKe17i96d2HoCpvmZu8")
 #geocode("1600 Amphitheatre Parkway, Mountain View, CA", output = "latlon", source = "google")
 
-locations0 <- read_xlsx("data/slack/keystone/locations.xlsx")%>%
-  mutate(City = "Chicago",
-         State = "IL",
-         pct_n = round((n/sum(n, na.rm = TRUE))*100, 2))
 locations1 <- locations0%>%
   mutate(Address = paste(Name, City, State, sep = ", "))
- check0 <- locations1%>%
+check0 <- locations1%>%
   filter(is.na(Address) | Address == "")
  addresses <- locations1$Address
  geocode_results <- geocode(addresses, output = "all", source = "google")
@@ -162,3 +172,24 @@ locations_sf0 <- locations_geocoded%>%
   st_as_sf(coords = c("lon", "lat"), crs = 4326)
 mapview(locations_sf0)
 st_write(locations_sf0, "data/slack/keystone/locations_geocoded.shp")
+
+
+
+
+# coalition food distributions --------------------------------------------
+#read in files from data portal to export into shapes
+distroNeighborhoods = c("ROGERS PARK", "NEAR WEST SIDE", "HUMBOLDT PARK", "BELMONT CRAGIN", "EDGEWATER", "LINCOLN SQURE", "RAVENSWOOD", 
+                        "PILSEN", "IRVING PARK", "EAST GARFIELD PARK", "AUSTIN", "NORTH LAWNDALE", "WEST GARFIELD PARK", "WEST LOOP")
+neighborhood <- read_delim("data/csv/Boundaries_-_Community_Areas_20250502.csv", delim = ",")%>%
+  mutate(cfsc_foodDistro_flag = case_when(COMMUNITY %in% distroNeighborhoods ~ "1",
+                                          TRUE ~ "0"))%>%
+  st_as_sf(wkt = "the_geom", crs = 4326)
+st_write(neighborhood, "data/shapefile/neighborhood.shp")
+neighborhood_centroid <- neighborhood%>%
+  st_centroid()
+check0 <- mapview(neighborhood) + mapview(neighborhood_centroid)
+check0
+st_write(neighborhood_centroid, "data/shapefile/neighborhood_centroid.shp")
+
+#export into geojson
+
